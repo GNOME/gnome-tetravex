2b8b2991bf75441c23f8292ba6e39df34e75a6e0
Callum McKenzie <callum@src.gnome.org>
2005-03-23 10:37:07 +0000

    The great patch fest of March 2005. Lots of HIG fixes. Menu beautification. More intelligent mine placement in gnomine.

--- gnotravex.c
+++ gnotravex.c
@@ -199,16 +199,16 @@ GnomeUIInfo size_radio_list[] = {
 
 GnomeUIInfo move_menu[] = {
   {GNOME_APP_UI_ITEM, N_("_Up"), N_("Move the pieces up"),
-   move_cb, "n", NULL, GNOME_APP_PIXMAP_NONE, NULL, 0, 0, NULL},
+   move_cb, "n", NULL, GNOME_APP_PIXMAP_STOCK, GTK_STOCK_GO_UP, 0, 0, NULL},
 
   {GNOME_APP_UI_ITEM, N_("_Left"), N_("Move the pieces left"),
-   move_cb, "w", NULL, GNOME_APP_PIXMAP_NONE, NULL, 0, 0, NULL},
+   move_cb, "w", NULL, GNOME_APP_PIXMAP_STOCK, GTK_STOCK_GO_BACK, 0, 0, NULL},
 
   {GNOME_APP_UI_ITEM, N_("_Right"), N_("Move the pieces right"),
-   move_cb, "e", NULL, GNOME_APP_PIXMAP_NONE, NULL, 0, 0, NULL},
+   move_cb, "e", NULL, GNOME_APP_PIXMAP_STOCK, GTK_STOCK_GO_FORWARD, 0, 0, NULL},
 
   {GNOME_APP_UI_ITEM, N_("_Down"), N_("Move the pieces down"),
-   move_cb, "s", NULL, GNOME_APP_PIXMAP_NONE, NULL, 0, 0, NULL},
+   move_cb, "s", NULL, GNOME_APP_PIXMAP_STOCK, GTK_STOCK_GO_DOWN, 0, 0, NULL},
 
   GNOMEUIINFO_END
 };
