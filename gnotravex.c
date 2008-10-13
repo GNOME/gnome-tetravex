0d3d935d47b43ee45567e6e6315204525e96fd14
Thomas Hindoe Paaboel Andersen <thomashpa@src.gnome.org>
2008-10-13 15:54:06 +0000

    replace gnome_help_display with gtk_show_uri
    svn path=/trunk/; revision=8013

--- gnotravex.c
+++ gnotravex.c
@@ -1884,11 +1884,26 @@ solve_cb (GtkAction * action, gpointer data)
 void
 help_cb (GtkAction * action, gpointer data)
 {
-#ifdef HAVE_GNOME
-  gnome_help_display ("gnotravex.xml", NULL, NULL);
-#else
-#warning need to port help to gtk-only!
-#endif /* HAVE_GNOME */
+  GdkScreen *screen;
+  GError *error = NULL;
+
+  screen = gtk_widget_get_screen (GTK_WIDGET (window));
+  gtk_show_uri (screen, "ghelp:gnotravex", gtk_get_current_event_time (), &error);
+
+  if (error != NULL)
+  {
+    GtkWidget *d;
+    d = gtk_message_dialog_new (GTK_WINDOW (window), 
+                              GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                              GTK_MESSAGE_ERROR, GTK_BUTTONS_CLOSE, 
+                              "%s", _("Unable to open help file"));
+    gtk_message_dialog_format_secondary_text (GTK_MESSAGE_DIALOG (d),
+                              "             %s", error->message);
+    g_signal_connect (d, "response", G_CALLBACK (gtk_widget_destroy), NULL);
+    gtk_window_present (GTK_WINDOW (d));
+
+    g_error_free (error);
+  }
 }
 
 void
